f3f8886a9f63b83e8ff0be5c9863f6fba97b0545
antoniamayaobrador <antoni.amaya.obrador@gmail.com>
2025-05-18 21:20:46 +0200

    message

--- weather/dbt.py
+++ weather/dbt.py
@@ -1,34 +1,31 @@
 from pathlib import Path
 import os
-from dagster import AssetExecutionContext
+from dagster import AssetExecutionContext, EnvVar
 from dagster_dbt import DbtCliResource, dbt_assets
 
-# Carga de variables de entorno
-DBT_PROJECT_DIR = os.getenv("DBT_PROJECT_DIR")
-DBT_PROFILES_DIR = os.getenv("DBT_PROFILES_DIR")
+# Usa Entorno en cloud, pero fallback en local
+DBT_PROJECT_DIR = os.getenv("DBT_PROJECT_DIR", "/Users/toniamayaobrador/dbt_weather_dagster/weather/weather_project")
+DBT_PROFILES_DIR = os.getenv("DBT_PROFILES_DIR", "/Users/toniamayaobrador/dbt_weather_dagster/weather/weather_project")
 
-# Validaciones para facilitar el debug
-if not DBT_PROJECT_DIR or not os.path.exists(DBT_PROJECT_DIR):
+# Validación para debugging
+if not Path(DBT_PROJECT_DIR).exists():
     raise ValueError(f"DBT_PROJECT_DIR no existe o no está definido: {DBT_PROJECT_DIR}")
-
-if not DBT_PROFILES_DIR or not os.path.exists(DBT_PROFILES_DIR):
+if not Path(DBT_PROFILES_DIR).exists():
     raise ValueError(f"DBT_PROFILES_DIR no existe o no está definido: {DBT_PROFILES_DIR}")
 
-# Path al manifest.json
 manifest_path = Path(DBT_PROJECT_DIR) / "target" / "manifest.json"
 if not manifest_path.exists():
     raise FileNotFoundError(f"manifest.json no encontrado en: {manifest_path}")
 
-# Recurso de dbt
+# Recurso dbt
 dbt = DbtCliResource(
     project_dir=DBT_PROJECT_DIR,
     profiles_dir=DBT_PROFILES_DIR,
 )
 
-# Definición de assets con el manifest generado previamente
+# Assets
 @dbt_assets(manifest=str(manifest_path))
 def weather_project_dbt_assets(context: AssetExecutionContext, dbt: DbtCliResource):
-    """Ejecuta `dbt build` con el manifiesto precompilado."""
     target = os.getenv("DBT_TARGET", "dev")
-    context.log.info(f"Ejecutando `dbt build` con target: {target}")
+    context.log.info(f"Running dbt build with target: {target}")
     yield from dbt.cli(["build", "--target", target], context=context).stream()
